e1e2a0816af688e533d6c7b7e0ad01d91ffff12e
Sergio Pérez <sergio.perez@albatronic.com>
2014-08-10 03:41:28 +0200

    Commit Global

--- modules/Rss/RssController.class.php
+++ modules/Rss/RssController.class.php
@@ -23,24 +23,28 @@ class RssController extends ControllerProject {
                 $rows = $contenido->cargaCondicion("Titulo,Resumen,UrlFriendly,PublishedAt", "BlogPublicar='1'", "PublishedAt DESC {$limit}");
                 unset($contenido);
                 break;
+            
             case 'noticias':
                 $titulo = "Noticias";
                 $contenido = new GconContenidos();
                 $rows = $contenido->cargaCondicion("Titulo,Resumen,UrlFriendly,PublishedAt", "NoticiaPublicar='1'", "PublishedAt DESC {$limit}");
                 unset($contenido);
                 break;
+            
             case 'eventos':
                 $titulo = "Eventos";
                 $contenido = new GconContenidos();
                 $rows = $contenido->cargaCondicion("Titulo,Resumen,UrlFriendly,PublishedAt", "EsEvento='1'", "PublishedAt DESC {$limit}");
                 unset($contenido);
                 break;
+            
             case 'wiki':
                 $titulo = "Wiki";
                 $contenido = new GconContenidos();
                 $rows = $contenido->cargaCondicion("Titulo,Resumen,UrlFriendly,PublishedAt", "EsWiki='1'", "PublishedAt DESC {$limit}");
                 unset($contenido);
                 break;
+            
             case 'categorias':
                 if (class_exists("Familias")) {
                     $titulo = "Categorias de Productos";
@@ -49,6 +53,7 @@ class RssController extends ControllerProject {
                     unset($familia);
                 }
                 break;
+                
             case 'productos':
                 if (class_exists("Articulos")) {
                     $titulo = "Productos";
@@ -57,37 +62,16 @@ class RssController extends ControllerProject {
                     unset($producto);
                 }
                 break;
+                
+            case 'contenidos':
             default:
                 $titulo = "Contenidos";
                 $contenido = new GconContenidos();
                 $rows = $contenido->cargaCondicion("Titulo,Resumen,UrlFriendly,PublishedAt", "1", "PublishedAt DESC {$limit}");
-                unset($contenido);
+                unset($contenido);                
         }
 
-        $items = "";
-        foreach ($rows as $row) {
-
-            $items .= "<item>"
-                    . "<guid isPermaLink='true'>{$_SESSION['appUrl']}{$row['UrlFriendly']}</guid>"
-                    . "<title>{$row['Titulo']}</title>"
-                    . "<link>{$_SESSION['appUrl']}{$row['UrlFriendly']}</link>"
-                    . "<description><![CDATA[{$row['Resumen']}]]></description>"
-                    . "<pubDate>" . date('r', strtotime($row['PublishedAt'])) . "</pubDate>"
-                    . "</item>";
-        }
-
-        $xml = '<?xml version="1.0" encoding="UTF-8" ?>'
-                . "<rss version=\"2.0\" xmlns:atom=\"http://www.w3.org/2005/Atom\">"
-                . "<channel>"
-                . "<title>RSS {$_SESSION['varWeb']['Pro']['globales']['empresa']} - {$titulo}</title>"
-                . "<link>{$_SESSION['appUrl']}/rss</link>"
-                . "<description>RSS {$_SESSION['varWeb']['Pro']['globales']['empresa']}</description>"
-                . "<language>es</language>"
-                . "<atom:link href=\"{$_SESSION['appUrl']}/rss\" rel=\"self\" type=\"application/rss+xml\" />"
-                . $items
-                . "</channel></rss>";
-
-        $this->values['rss'] = $xml;
+        $this->values['rss'] = Feeds::getRss($titulo,$rows);
 
         header('Content-type: application/xml; charset="UTF-8"', true);
         //echo $xml;
